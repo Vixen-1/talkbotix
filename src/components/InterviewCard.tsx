d17d1188b6f06dc284c2b8ab269ab3ce4b42ea05
Ayushi <ayushisaxena24111999@gmail.com>
2025-07-23 17:09:49 +0530

    added on main screen

--- src/components/InterviewCard.tsx
+++ src/components/InterviewCard.tsx
@@ -5,8 +5,9 @@ import { Button } from "./ui/button";
 import Link from "next/link";
 import { getRandomInterviewCover } from "@/lib/utils";
 import DisplayTechIcons from "./DisplayTechIcons";
+import { getFeedbackByInterviewId } from "@/lib/actions/general.action";
 
-const InterviewCard = ({
+const InterviewCard = async ({
   interviewId,
   userId,
   role,
@@ -14,7 +15,7 @@ const InterviewCard = ({
   techstack,
   createdAt,
 }: InterviewCardProps) => {
-  const feedback = null as Feedback | null;
+  const feedback = userId && interviewId ? await getFeedbackByInterviewId({interviewId, userId}) : null;
   const normalizedType = /mix/gi.test(type) ? `Mixed` : type;
   const formattedDate = dayjs(
     feedback?.createdAt || createdAt || Date.now()
